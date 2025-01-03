08eb2349f618cb8dd84f6e47a9140464806dbc05
avoidme12 <zaharovds10@gmail.com>
2025-01-03 23:03:30 +1000

    first commit

---
+++ src/assets/assets.js
@@ -0,0 +1,290 @@
+import bell_icon from './bell.png'
+import home_icon from './home.png'
+import like_icon from './like.png'
+import loop_icon from './loop.png'
+import mic_icon from './mic.png'
+import next_icon from './next.png'
+import play_icon from './play.png'
+import pause_icon from './pause.png'
+import plays_icon from './plays.png'
+import prev_icon from './prev.png'
+import search_icon from './search.png'
+import shuffle_icon from './shuffle.png'
+import speaker_icon from './speaker.png'
+import stack_icon from './stack.png'
+import zoom_icon from './zoom.png'
+import plus_icon from './plus.png'
+import arrow_icon from './arrow.png'
+import mini_player_icon from './mini-player.png'
+import queue_icon from './queue.png'
+import volume_icon from './volume.png'
+import arrow_right from './right_arrow.png'
+import arrow_left from './left_arrow.png'
+import spotify_logo from './spotify_logo.png'
+import clock_icon from './clock_icon.png'
+import dark_beach from './Dark Beach.mp3'
+import darkBeachLogo from './dark beach logo.jpg'
+import limerenceImg from './limerence.jpg'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import snowfallImg from './snowfall.jpg'
+import img9 from './img9.jpg'
+import ineyimg from './иней.jpg'
+import yuppie from './yippee-tbh.mp3'
+import snowfall from './snowfall.mp3'
+import farFromHome from './farFromHome.mp3'
+import theWorstDays from './the worst days.mp3'
+import limerence from './limerence.mp3'
+import rainOnMyWindow from './rain-on-my-window.mp3'
+import indexFromUs from './index-of-us-slowed-reverb.mp3'
+import ineySong from './ΔXIUS LIИK,ножевые ранения - NНΞЙ.mp3'
+
+
+export const assets = {
+    bell_icon,
+    home_icon,
+    like_icon,
+    loop_icon,
+    mic_icon,
+    next_icon,
+    play_icon,
+    plays_icon,
+    prev_icon,
+    search_icon,
+    shuffle_icon,
+    speaker_icon,
+    stack_icon,
+    zoom_icon,
+    plus_icon,
+    arrow_icon,
+    mini_player_icon,
+    volume_icon,
+    queue_icon,
+    pause_icon,
+    arrow_left,
+    arrow_right,
+    spotify_logo,
+    clock_icon,
+    yuppie,
+}
+
+export const albumsData = [
+    {   
+        id:0,
+        name: "Ambient playlist",
+        image: img1,
+        desc:"Most relaxed music",
+        bgColor:"#033559"
+    },
+]
+
+export const songsData = [
+    {
+        id:0,
+        name: "Dark Beach",
+        image: darkBeachLogo,
+        file: dark_beach,
+        desc: 'By Pastel Ghost',
+        minutes: '4',
+        seconds: '22',
+    },
+    {
+        id:1,
+        name: "Far From Home",
+        image: img2,
+        file:farFromHome,
+        desc:"By deneb",
+        minutes: '2',
+        seconds: '46',
+    },
+    {
+        id:2,
+        name: "The worst days",
+        image: img3,
+        file:theWorstDays,
+        desc:"By remind me",
+        minutes: '2',
+        seconds: '08',
+    },
+    {
+        id:3,
+        name: "Limerence",
+        image: limerenceImg,
+        file:limerence,
+        desc:"By Øneheart",
+        minutes: '1',
+        seconds: '57',
+    },
+    {
+        id:4,
+        name: "rain on my window.",
+        image: img5,
+        file:rainOnMyWindow,
+        desc:"By undercurrent",
+        minutes: '1',
+        seconds: '44',
+    },
+    {
+        id:5,
+        name: "index for us",
+        image: img6,
+        file:indexFromUs,
+        desc:"By anębu",
+        minutes: '1',
+        seconds: '36',
+    },
+    {
+        id:6,
+        name: "Snowfall",
+        image: snowfallImg,
+        file:snowfall,
+        desc:"By Øneheart, Reidenshi",
+        minutes: '2',
+        seconds: '04',
+    },
+    {
+        id:7,
+        name: "NНΞЙ",
+        image: ineyimg,
+        file: ineySong,
+        desc:"By ΔXIUS LIИK, ножевые ранения",
+        minutes: '4',
+        seconds: '13',
+    },
+]
+
+export const mostPopularMusic = [
+    {
+        id:0,
+        name: "Dark Beach",
+        image: darkBeachLogo,
+        file: dark_beach,
+        desc: 'By Pastel Ghost',
+        minutes: '4',
+        seconds: '22',
+    },
+    {
+        id:1,
+        name: "Far From Home",
+        image: img2,
+        file:farFromHome,
+        desc:"By deneb",
+        minutes: '2',
+        seconds: '46',
+    },
+    {
+        id:2,
+        name: "The worst days",
+        image: img3,
+        file:theWorstDays,
+        desc:"By remind me",
+        minutes: '2',
+        seconds: '08',
+    },
+    {
+        id:3,
+        name: "Limerence",
+        image: limerenceImg,
+        file:limerence,
+        desc:"By Øneheart",
+        minutes: '1',
+        seconds: '57',
+    },
+    {
+        id:4,
+        name: "rain on my window.",
+        image: img5,
+        file:rainOnMyWindow,
+        desc:"By undercurrent",
+        minutes: '1',
+        seconds: '44',
+    },
+    {
+        id:5,
+        name: "index for us",
+        image: img6,
+        file:indexFromUs,
+        desc:"By anębu",
+        minutes: '1',
+        seconds: '36',
+    },
+    {
+        id:6,
+        name: "Snowfall",
+        image: snowfallImg,
+        file:snowfall,
+        desc:"By Øneheart, Reidenshi",
+        minutes: '2',
+        seconds: '13',
+    },
+]
+
+export const relaxedSongsData = [
+    {
+        id:0,
+        name: "Dark Beach",
+        image: darkBeachLogo,
+        file: dark_beach,
+        desc: 'By Pastel Ghost',
+        minutes: '4',
+        seconds: '22',
+    },
+    {
+        id:1,
+        name: "Far From Home",
+        image: img2,
+        file:farFromHome,
+        desc:"By deneb",
+        minutes: '2',
+        seconds: '46',
+    },
+    {
+        id:2,
+        name: "The worst days",
+        image: img3,
+        file:theWorstDays,
+        desc:"By remind me",
+        minutes: '2',
+        seconds: '08',
+    },
+    {
+        id:3,
+        name: "Limerence",
+        image: limerenceImg,
+        file:limerence,
+        desc:"By Øneheart",
+        minutes: '1',
+        seconds: '57',
+    },
+    {
+        id:4,
+        name: "rain on my window.",
+        image: img5,
+        file:rainOnMyWindow,
+        desc:"By undercurrent",
+        minutes: '1',
+        seconds: '44',
+    },
+    {
+        id:5,
+        name: "index for us",
+        image: img6,
+        file:indexFromUs,
+        desc:"By anębu",
+        minutes: '1',
+        seconds: '36',
+    },
+    {
+        id:6,
+        name: "Snowfall",
+        image: snowfallImg,
+        file:snowfall,
+        desc:"By Øneheart, Reidenshi",
+        minutes: '2',
+        seconds: '13',
+    },
+]
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import React, {useContext, useRef, useState} from 'react';
+import {HomeIcon, SearchIcon} from "lucide-react";
+import {PlayerContext} from "../context/PlayerContext.tsx";
+import {assets} from "@/assets/assets";
+
+export default function Sidebar(){
+
+    const [cat, setCat] = useState(false)
+    const yuppieRef = useRef(null)
+    const {playWithId, audioRef,track,play,mute} = useContext(PlayerContext)
+
+    return(
+        <div className='w-[25%] h-full p-2 flex-col gap-2 text-white lg:flex'>
+            <div className='bg-[#121212] h-[20%] rounded md:flex flex-col justify-around'>
+                <div className='items-center gap-3 pl-8 cursor-pointer grid grid-cols-1'>
+                    <HomeIcon className='w-6 flex left-6'/>
+                    <p className='font-bold'>Домашняя страница</p></div>
+                <div className='md:shrink-0 items-center gap-3 pl-8 cursor-pointer grid grid-cols-1'>
+                    <SearchIcon className='w-6'/>
+                    <p className='font-bold'>Поиск</p>
+                </div>
+            </div>
+            {cat
+                ? <div className='m-4 flex items-center gap-3 pl-8 transition-all ease-in duration-150'>
+                    <img
+                        className='w-[90px] rounded-lg'
+                        width={500}
+                        height={500}
+                        src='https://media1.tenor.com/m/sVNO62-MYV0AAAAC/zxc-cat.gif'
+                        alt='cat'/>
+                </div>
+                : ''}
+            <div
+                className='absolute mt-[75vh] justify-center items-center gap-3 pl-1 cursor-pointer text-gray-300 hover:text-white transition-all ease-in duration-150'
+                onClick={() => {
+                    setCat(!cat)
+                    yuppieRef.current.play()
+                    console.log(playWithId)
+                    console.log(audioRef)
+                    console.log(track)
+                    console.log(play)
+                    console.log(mute)
+                }}
+            >
+                <h1 className='ml-5 font-semibold'>By Avo1d</h1>
+                <img
+                    className='w-[60px] ml-5 lg:mb-5 mt-[10px]'
+                    width={500}
+                    height={500}
+                    alt='img'
+                    src='https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRFLGgvp-goECgdN65x6oW1Hx-2XJKT_PkTIB_Pz0xJtTIwnuV8'
+                />
+            </div>
+            <audio ref={yuppieRef} src={assets.yuppie} preload='auto'></audio>
+        </div>
+    )
+}
